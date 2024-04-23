0e566a8856e34e07160add13ef8951c2b70d2419
Junior Gagne <damoncaggne2002@gmail.com>
2024-04-23 12:12:49 +0200

    création du projet principal

---
+++ src/main/java/org/myproject/ebiblio/EbiblioApplication.java
@@ -0,0 +1,13 @@
+package org.myproject.ebiblio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbiblioApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EbiblioApplication.class, args);
+    }
+
+}
